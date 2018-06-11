9c43fa17f7e6bed732fed836087cdebdbb1d6b34
Travis Smith <Travis.Smith2@gmail.com>
2018-06-11 18:58:49 -0400

    character generator improvements

--- public/assets/js/characterGenerator.js
+++ public/assets/js/characterGenerator.js
@@ -1,30 +1,65 @@
 var race = Math.floor(Math.random() * 9) + 1;
 var profession = Math.floor(Math.random() * 12) + 1;
-var gender = Math.floor(Math.random() * 2) +1;
+var gender = Math.floor(Math.random() * 2) + 1;
+var currentGender;
+var sr;
+var newSR;
+var currentSR;
+var currentJob;
+var newCharacter;
+var charName;
 
+var request = require("request");
 
-var request = require('request');
-
-request('http://www.dnd5eapi.co/api/races/' + race, function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+request("http://www.dnd5eapi.co/api/races/" + race, function(
+  error,
+  response,
+  body
+) {
+  console.log("error:", error); // Print the error if one occurred
+  console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
   var currentRace = JSON.parse(body);
-  console.log('-------------------------------------');
-  console.log('Race:', currentRace.name);
-  if(gender === 1) {
-      console.log('Gender: Male');
-  } else {
-      console.log('Gender: Female');
+  console.log("-------------------------------------");
+  console.log("Race:", currentRace.name);
+  if (currentRace.hasOwnProperty("subraces")) {
+    if (currentRace.subraces.length === 0) {
+      console.log("Subrace: N/A");
+      currentSR = "N/A"
+    } else if (currentRace.subraces.length === 1) {
+      newSR = currentRace.subraces[0];
+      console.log("Subrace:", newSR.name);
+      currentSR = newSR.name;
+    } else {
+      sr = Math.floor(Math.random() * currentRace.subraces.length);
+      newSR = currentRace.subraces[sr];
+      console.log("Subrace:", newSR.name);
+      currentSR = newSR.name;
+    }
   }
-  request('http://www.dnd5eapi.co/api/classes/' + profession, function (error, response, body) {
-    var currentJob = JSON.parse(body);
-    console.log('Job:', currentJob.name);
+  if (gender === 1) {
+    console.log("Gender: Male");
+    currentGender = "Male";
+  } else {
+    console.log("Gender: Female");
+    currentGender = "Female";
+  }
+  request("http://www.dnd5eapi.co/api/classes/" + profession, function(
+    error,
+    response,
+    body
+  ) {
+    var newJob = JSON.parse(body);
+    console.log("Job:", newJob.name);
+    currentJob = newJob.name;
 
-})
-})
+    newCharacter = {
+        characterName: charName,
+        characterGender: currentGender,
+        characterRace: currentRace.name,
+        characterSR: currentSR,
+        characterJob: currentJob
+      };
+      console.log(newCharacter);
+  });
 
-request('http://www.dnd5eapi.co/api/classes/' + profession, function (error, response, body) {
-    var currentJob = JSON.parse(body);
-    console.log('Job:', currentJob.name);
-
-})
+});
