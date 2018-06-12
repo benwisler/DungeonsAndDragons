3d736d9a418a5d54394d4c5ddeb60e461a858d73
Travis Smith <Travis.Smith2@gmail.com>
2018-06-11 21:27:35 -0400

    added Proficiencies to chargen

--- public/assets/js/characterGenerator.js
+++ public/assets/js/characterGenerator.js
@@ -1,5 +1,7 @@
 var race = Math.floor(Math.random() * 9) + 1;
+var currentRace;
 var profession = Math.floor(Math.random() * 12) + 1;
+var newJob;
 var gender = Math.floor(Math.random() * 2) + 1;
 var currentGender;
 var sr;
@@ -13,6 +15,8 @@ var modAttr = [];
 var combAttr = [];
 var hp;
 var charHitDie;
+var charProf = [];
+var choiceArray = [];
 
 var request = require("request");
 
@@ -23,7 +27,7 @@ request("http://www.dnd5eapi.co/api/races/" + race, function(
 ) {
   console.log("error:", error); // Print the error if one occurred
   console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-  var currentRace = JSON.parse(body);
+  currentRace = JSON.parse(body);
   modAttr = currentRace.ability_bonuses;
   baseAttr = [15, 14, 13, 12, 10, 8];
   for (var i = 0; i < 6; i++) {
@@ -62,30 +66,62 @@ request("http://www.dnd5eapi.co/api/races/" + race, function(
     response,
     body
   ) {
-    var newJob = JSON.parse(body);
+    newJob = JSON.parse(body);
     console.log("Job:", newJob.name);
     currentJob = newJob.name;
     charHitDie = newJob.hit_die;
     hp = charHitDie + combAttr[2];
     console.log("HP:",hp);
 
+    var profLength = currentRace.starting_proficiencies.length;
+    for (var i = 0; i < profLength; i++) {
+      var cp = currentRace.starting_proficiencies[i];
+      charProf.push(cp.name);
+    }
+
+    if (currentRace.hasOwnProperty("starting_proficiency_options")) {
+      var profOpsLength = currentRace.starting_proficiency_options.from.length;
+      console.log(profOpsLength);
+      var numProfOps = currentRace.starting_proficiency_options.choose;
+      console.log(numProfOps);
+      var rprof = Math.floor(Math.random() * profOpsLength);
+      var newrprof = currentRace.starting_proficiency_options.from[rprof];
+      console.log(newrprof.name);
+      charProf.push(newrprof.name);
+    }
+    var classProfLength = newJob.proficiencies.length;
+    for (var i = 0; i < classProfLength; i++) {
+      var cp = newJob.proficiencies[i];
+      charProf.push(cp.name);
+    }
+    var choices = newJob.proficiency_choices[0];
+    var numChoices = choices.choose;
+    console.log(numChoices);
+    console.log(choices.from.length);
+    for (var i = 0; i < choices.from.length; i++) {
+      choiceArray.push(choices.from[i].name);
+    }
+    console.log(choiceArray.length);
+    var chosenArray = [];
+    for (var i = 0; i < numChoices; i++) {
+      var rnd = Math.floor(Math.random() * (choiceArray.length - 1));
+      chosenArray.push(choiceArray[rnd]);
+      choiceArray.splice(rnd, 1);
+    }
+    for (var i = 0; i < chosenArray.length; i++) {
+      charProf.push(chosenArray[i]);
+    }
 
 
 
-
-
-
-
-
-
-    
     newCharacter = {
       characterHP: hp,
       characterGender: currentGender,
       characterRace: currentRace.name,
       characterSR: currentSR,
       characterJob: currentJob,
-      characterAttr: combAttr
+      characterAttr: combAttr,
+      characterProf: charProf
     };
     console.log(newCharacter);
   });
