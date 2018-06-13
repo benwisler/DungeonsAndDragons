8b337b3b9bde1a39a3b26c65e64ea81617a56a20
Ben <bwisler95@gmail.com>
2018-06-13 18:43:21 -0400

    server's working

--- public/js/login.js
+++ public/js/login.js
@@ -1,33 +1,75 @@
-function login(username, password, callback) {
-  var connection = mysql({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'users'
-  });
-
-  connection.connect();
-
-  var query = "SELECT id, username, password " +
-    "FROM users WHERE username = ?";
-
-  connection.query(query, [username], function (err, results) {
-    if (err) return callback(err);
-    if (results.length === 0) return callback(new WrongUsernameOrPasswordError(email));
-    var user = results[0];
-
-    bcrypt.compare(password, user.password, function (err, isValid) {
-      if (err) {
-        callback(err);
-      } else if (!isValid) {
-        callback(new WrongUsernameOrPasswordError(username));
-      } else {
-        callback(null, {
-          id: user.id.toString(),
-          username: user.username,
-        });
+$(document).ready(function() {
+    // Getting references to our form and inputs
+    var username = $("#username");
+    var password = $("#password");
+  
+    // When the form is submitted, we validate there's an email and password entered
+    $("#login").on("click", function(event) {
+      event.preventDefault();
+      var userData = {
+        username: username.val().trim(),
+        password: password.val().trim()
+      };
+  
+      if (!userData.username || !userData.password) {
+        return;
       }
+  
+      // If we have an email and password we run the loginUser function and clear the form
+      loginUser(userData.username, userData.password);
+      username.val("");
+      password.val("");
     });
-
+  
+    // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+    function loginUser(username, password) {
+      $.post("/api/login", {
+        username: username,
+        password: password
+      }).then(function(data) {
+        window.location.replace(data);
+        // If there's an error, log the error
+      }).catch(function(err) {
+        console.log(err);
+      });
+    }
+  
   });
-}
+// $(document).ready(function() {
+//     // Getting references to our form and inputs
+//     var loginForm = $("form.login");
+//     var username = $("#username");
+//     var password = $("#password");
+  
+//     // When the form is submitted, we validate there's an email and password entered
+//     $("#login").on("click", function(event) {
+//       event.preventDefault();
+//       var userData = {
+//         username: username.val().trim(),
+//         password: password.val().trim()
+//       };
+  
+//       if (!userData.username || !userData.password) {
+//         return;
+//       }
+  
+//       // If we have an email and password we run the loginUser function and clear the form
+//       loginUser(userData.username, userData.password);
+//       username.val("");
+//       password.val("");
+//     });
+  
+//     // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+//     function loginUser(username, password) {
+//       $.post("/api/login", {
+//         username: username,
+//         password: password
+//       }).then(function(data) {
+//         window.location.replace(data);
+//         // If there's an error, log the error
+//       }).catch(function(err) {
+//         console.log(err);
+//       });
+//     }
+  
+//   });
