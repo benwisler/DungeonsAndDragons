9dc6163e22cfbff5eee2f5f94d59d64da9491d12
Stacy M <geekgirlbeta@gmail.com>
2018-06-14 13:15:28 -0400

    creatAccount html

--- server.js
+++ server.js
@@ -1,32 +1,49 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-
-var PORT = process.env.PORT || 8080;
-
+var express = require('express');
 var app = express();
-
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// parse application/json
+var passport = require('passport')
+var session = require('express-session')
+var bodyParser = require('body-parser')
+var env = require('dotenv').load();
+//For BodyParser
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
+// For Passport
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+})); // session secret
+require("./app/routes/api-routes.js")(app);
+require("./app/routes/html-routes.js")(app);
+app.use(passport.initialize());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+app.use(passport.session()); // persistent login sessions
+app.get('/', function (req, res) {
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+  res.redirect("/login");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/dndController.js");
+});
 
-app.use(routes);
+//Models
+var models = require("./app/models");
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+//Sync Database
+models.sequelize.sync().then(function () {
+
+  console.log('Nice! Database looks fine')
+
+}).catch(function (err) {
+
+  console.log(err, "Something went wrong with the Database Update!")
+
+});
+
+app.listen(8080, function (err) {
+
+  if (!err)
+    console.log("Site is live");
+  else console.log(err)
+
 });
