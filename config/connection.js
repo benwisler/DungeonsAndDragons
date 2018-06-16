965d7947bb3947627e165a4f30ebafb3e0b5911c
Travis Smith <Travis.Smith2@gmail.com>
2018-06-16 11:40:16 -0400

    heroku jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,4 +2,6 @@ var connection;
 if(process.env.JAWSDB_URL) {
   //Heroku deployment
     connection = mysql.createConnection(process.env.JAWSDB_URL);
-} 
+} else {
+    connection = require("config.json");
+}
